4b2a16bb213303455d8bf128a2d28648bd9eabfe
Ritavdas <ritavdas@gmail.com>
2020-08-07 11:42:20 +0530

    addition command

---
+++ add.py
@@ -0,0 +1,12 @@
+a = int(input("Enter one Number: "))
+b = int((input("Enter second Number: ")))
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+print(add(a, b))
+
+add_result = add(a, b)
+print(f'Result of addition of {a} and {b} =  {add_result}')
